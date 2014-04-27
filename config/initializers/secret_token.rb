9ae128e160c39a7888164bc3f11b8347752880f2
Vishal Sharma <vishalsh@thoughtworks.com>
2014-04-27 14:42:14 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssetMgmt::Application.config.secret_key_base = '045b271dfb54d9e8c77a767dc5c119111a8bea11d3ea3489e02b5607b465e9d787db9dcb73fa6b4940c9a13470735c4bbbaa9f48d7cb8cf58978f48a96d6419b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AssetMgmt::Application.config.session_store :cookie_store, key: '_asset-mgmt_session'

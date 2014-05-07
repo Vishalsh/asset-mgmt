3ddffcf18adad9adecfd554d00f5d7206cee2fb2
Vishal Sharma <vishalsh@thoughtworks.com>
2014-05-07 11:50:07 +0530

    Removed slim templating

--- app/assets/javascripts/backbone/views/asset_type_view.js
+++ app/assets/javascripts/backbone/views/asset_type_view.js
@@ -7,7 +7,7 @@ App.Views.Asset_type_list = Backbone.View.extend({
         var asset_types = new App.Collections.Asset_type;
         asset_types.fetch({
             success: function (asset_types) {
-                var template = _.template($('#asset_type_list').html(), {asset_types: asset_types.models});
+                var template = _.template($('#asset_type_list').html(), {asset_types: asset_types.models, list_assets: 'active', new_asset: ''});
                 self.$el.html(template);
             }
         })
@@ -20,7 +20,7 @@ App.Views.Asset_type_new = Backbone.View.extend({
     el: '.page-content',
 
     render: function () {
-        var template = _.template($("#asset_type_new").html(), {})
+        var template = _.template($("#asset_type_new").html(), { list_assets: '', new_asset: 'active'})
         this.$el.html(template);
     }
 });

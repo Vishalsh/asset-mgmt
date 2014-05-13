7743e793c4cfb24979992eccc662e0c12aacaaa9
Vishal Sharma <vishalsh@thoughtworks.com>
2014-05-13 12:48:52 +0530

    Resolved conflicts

--- app/assets/javascripts/backbone/routers/asset_type_router.js
+++ app/assets/javascripts/backbone/routers/asset_type_router.js
@@ -16,10 +16,9 @@ App.Routers.Asset_type = Backbone.Router.extend({
         asset_type_edit_view.render({id: id});
     },
 
-    destroy: function(id) {
+    destroy: function (id) {
         var asset_type_delete = new App.Views.Asset_type_delete;
-        asset_type_delete.deleteAssetType({id: id})
+        asset_type_delete.deleteAssetType({id: id});
     }
-
 });
 

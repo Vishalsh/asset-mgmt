9a18d5a50c34127ba6c405fb31a56cf3ec8746c8
Vishal Sharma <vishalsh@thoughtworks.com>
2014-06-06 23:24:21 +0530

    Added model for assigment

---
+++ spec/factories/assignments.rb
@@ -0,0 +1,17 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+
+  factory :valid_assignment, class: Assignment do |f|
+    f.start_date '22-05-2014'
+    f.end_date '24-05-2014'
+    f.assigned_to 'vishalsh'
+  end
+
+  factory :invalid_assignment, class: Assignment do |f|
+    f.start_date ''
+    f.end_date '24-05-2014'
+    f.assigned_to 'vishalsh'
+  end
+
+end
